e215da6c5560f63f5b82e3c3ea2144d9417dd45b
Sumeya6 <sumeyabahiru55@gmail.com>
2026-08-04 08:24:24 -0700

    feat: integrate Tailwind CSS and update dependencies
    - Added Tailwind CSS and its Vite plugin to the project.
    - Updated package.json and package-lock.json to include Tailwind CSS dependencies.
    - Modified App.jsx to include a Tailwind CSS styled heading.
    - Updated index.css to import Tailwind CSS and removed custom styles.
    - Adjusted vite.config.js to include Tailwind CSS plugin.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
@@ -116,7 +117,7 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
