1dae4f40c6ae88763497d158b2bf9bd990b3de79
Suad Abrar <suadabrar44@gmail.com>
2026-09-18 09:38:21 +0300

    chore: add jest config for unit tests

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
     "**/tests/report.test.js",
     "**/tests/search.test.js",
     "**/tests/announcement.test.js",
+    "**/tests/session.test.js",
+    "**/tests/ticket.test.js",
+    "**/tests/assignment.test.js",
   ],
 };
